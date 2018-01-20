610c095637ea8a8c8731f9961bd07966a2b16b7d
reeFridge <reefridge@interfaced.ru>
2018-01-21 01:24:03 +0700

    Exporting provider builder

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as HLSServer } from './hls-server';
-export { default as BittorrentStreamProvider } from './hls-server/providers/bittorrent';
+export { BittorrentStreamProvider, BittorrentStreamProviderBuilder } from './hls-server/providers/bittorrent';
 export { default as IStreamProvider } from './hls-server/interfaces/i-stream-provider';
