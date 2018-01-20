2608def204bca658cb3bb993e8b0496520500002
reeFridge <reefridge@interfaced.ru>
2018-01-21 01:24:49 +0700

    Updated example using new implemented features (builder, filter) and added some comments

--- src/examples/torrent-stream.ts
+++ src/examples/torrent-stream.ts
@@ -1,15 +1,27 @@
 import * as http from "http";
 import * as TorrentStream from "torrent-stream";
-import { HLSServer, BittorrentStreamProvider } from '../';
+import * as mime from 'mime';
+import { HLSServer, BittorrentStreamProviderBuilder } from '../';
 
 const MAGNET = 'magnet:?xt=urn:btih:6CD6E0DD7EDF1D40D5475AB2C3B5B4B647741911';
 const engine = TorrentStream(MAGNET);
 const server = http.createServer();
 
-new HLSServer(server, {
-    path: '/play',
-    provider: new BittorrentStreamProvider(engine)
-});
+// URL pathname to streaming on
+const path = '/play';
+
+// Create stream provider
+const provider = new BittorrentStreamProviderBuilder()
+    .withFilter((file) => {
+        const regex = /^video\//g;
+        const mimeType = mime.getType(file.name);
+
+        return regex.test(mimeType);
+    })
+    .build(engine);
+
+// attach streaming middleware to the server
+new HLSServer(server, { path, provider });
 
 server.listen(1337);
 console.log('Open network stream in VLC: http://localhost:1337/play');
